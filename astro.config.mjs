7a6ba7ca95e2e6353a8744485951d69d9cbd0980
wuerion <almerux@gmail.com>
2024-09-08 13:33:02 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  outDir: 'dist',
-  vite: {
-    resolve: {
-      alias: {
-        // Asegúrate de que no haya rutas con caracteres inválidos
-        '@': '/src',
-      },
-    },
-  },
+  integrations: [tailwind()]
 });
